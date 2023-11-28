63d5ebfba3bd4d09bcb941f9955b9f864c7eecf7
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-11-27 19:40:03 -0500

    rendered spa packages on screen

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,15 +1,15 @@
-
-
-export default function Packages() {
-
-    return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                
-            </div>
+export default function Packages({ packages }) {
+  const display = packages.map((spaPackage, i) => {
+    return <li key={i}>{spaPackage}</li>;
+  });
+  return (
+    <div>
+      <div className="packages">
+        <div className="packagesHeader">
+          <h1>Our Packages</h1>
+          <ul>{display}</ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
